76476470f9d35cb2951561980bb7368676b9b15b
LE FORESTIER Erwan TGI/OLS <RKLT4620@orange.com>
2020-11-30 11:24:42 +0100

    c

--- tp/js/webworkers/exemple.js
+++ tp/js/webworkers/exemple.js
@@ -7,7 +7,7 @@ const noCorsUrl = "https://cors-anywhere.herokuapp.com/";
 let calUrl = "https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu";
 
 let cacheResources = [
-    "/webworkers/",
+    "/tp/js/webworkers/",
     noCorsUrl + calUrl
 ]
 

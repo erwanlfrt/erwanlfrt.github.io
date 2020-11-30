d473fc5c47a75736a8949d47cd9359a7e2bf170d
LE FORESTIER Erwan TGI/OLS <RKLT4620@orange.com>
2020-11-30 08:59:12 +0100

    commit

--- tp/js/webworkers/script.js
+++ tp/js/webworkers/script.js
@@ -1,15 +1,30 @@
+this.addEventListener("install", function(event) {
+    // Ici on utilise waitUntil qui attends une promesse pour valider l'installation
+    event.waitUntil(
+      caches.open("mon-site-cache-v1").then(function(cache) {
+        // On met en cache une liste d'URLs qui sont la fondation de notre app.
+        return cache.addAll([
+            "https://cors-anywhere.herokuapp.com/https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu",
+        ]);
+      })
+    );
+  });
+  
+
+
+
 this.addEventListener('fetch', function(event) {
     event.respondWith(
-      caches.match(event.request).catch(function() {
-        return fetch(event.request).then(function(response) {
-          return caches.open('v1').then(function(cache) {
-            cache.put(event.request, response.clone());
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache HIT, on retourne la réponse en cache.
+          if (response) {
             return response;
-          });  
-        });
-      }).catch(function() {
-        console.log("damn")
-        return caches.match('https://cors-anywhere.herokuapp.com/https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu');
-      })
+          }
+          
+          // Sinon on effectue la requête réellement et on retourne son contenu.
+          return fetch(event.request);
+        }
+      )
     );
   });

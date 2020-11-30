1878c11d508d0cae512e1a441a56abaf9521e300
LE FORESTIER Erwan TGI/OLS <RKLT4620@orange.com>
2020-11-30 10:15:18 +0100

    c

--- tp/js/webworkers/exemple.js
+++ tp/js/webworkers/exemple.js
@@ -2,6 +2,7 @@
 self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('v1').then(function(cache) {
+        console.log("on build")
         return cache.addAll([
           'https://cors-anywhere.herokuapp.com/https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu',
         ]);

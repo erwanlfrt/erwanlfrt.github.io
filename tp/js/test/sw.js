a5bc9718898e024fd0291c7af87bd442618cd5c4
LE FORESTIER Erwan TGI/OLS <RKLT4620@orange.com>
2020-11-30 13:47:19 +0100

    c

--- tp/js/test/sw.js
+++ tp/js/test/sw.js
@@ -32,6 +32,7 @@ self.addEventListener('install', function(event) {
           // and serve second one
           console.log("on passe dans le fetch")
           //console.log("response = "+response.text())
+          //response.text().then((text) =>{console.log("text = "+text)})
           let responseClone = response.clone();
           
           caches.open('v1').then(function (cache) {

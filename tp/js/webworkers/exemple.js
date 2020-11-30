3309aa723ecc0ff1a82c2ac3a8a13409e3047265
LE FORESTIER Erwan TGI/OLS <RKLT4620@orange.com>
2020-11-30 10:29:15 +0100

    c

--- tp/js/webworkers/exemple.js
+++ tp/js/webworkers/exemple.js
@@ -12,28 +12,10 @@ self.addEventListener('install', function(event) {
 
   console.log("hey ")
   
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(caches.match(event.request).then(function(response) {
-      // caches.match() always resolves
-      // but in case of success response will have value
-      console.log("dans le fetch")
-      if (response !== undefined) {
-        return response;
-      } else {
-        return fetch(event.request).then(function (response) {
-          // response may be used only once
-          // we need to save clone to put one copy in cache
-          // and serve second one
-          let responseClone = response.clone();
-          
-          caches.open('v1').then(function (cache) {
-            cache.put(event.request, responseClone);
-          });
-          console.log("res = "+JSON.stringify(response.text()))
-          return response.text();
-        }).catch(function () {
-          return caches.match('https://cors-anywhere.herokuapp.com/https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu');
-        });
-      }
-    }));
+  this.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).catch(function() {
+        return fetch(event.request);
+      })
+    );
   });

1047f2ee3521b2ae517545ba21a13b3cbb35e293
LE FORESTIER Erwan TGI/OLS <RKLT4620@orange.com>
2020-11-30 10:44:38 +0100

    c

--- tp/js/webworkers/exemple.js
+++ tp/js/webworkers/exemple.js
@@ -1,22 +1,27 @@
-  
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('v1').then(function(cache) {
-        console.log("on build")
-        return cache.addAll([
-          'https://cors-anywhere.herokuapp.com/https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu',
-        ]);
-      })
-    );
-  });
+const cacheName = "webworkers";
 
-  console.log("hey ")
-  
-  this.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then((response) =>{
-          console.log("ok for the answer")
-          return response || fetch(event.request)
-      })
+const noCorsUrl = "https://cors-anywhere.herokuapp.com/";
+
+let calUrl = "https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu";
+
+let cacheResources = [
+    "/tp/js/webworkers/",
+    noCorsUrl + calUrl
+]
+
+this.addEventListener("install", (event) =>{
+    event.waitUntil(
+        caches.open(cacheName).then((cache) =>{
+            return cache.addAll(cacheResources);
+        })
     );
-  });
+});
+
+this.addEventListener("fetch", (event) =>{
+    console.log("fetched");
+    event.respondWith(
+        caches.match(event.request).then((response) =>{
+            return response || fetch(event.request);
+        })
+    )
+})

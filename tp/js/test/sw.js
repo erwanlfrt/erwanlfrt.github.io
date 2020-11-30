5b8e9d6fdb42c4cbc05091a82d5fa36eaa4aa20a
LE FORESTIER Erwan TGI/OLS <RKLT4620@orange.com>
2020-11-30 13:17:42 +0100

    c

--- tp/js/test/sw.js
+++ tp/js/test/sw.js
@@ -2,7 +2,10 @@ const noCorsUrl = "https://cors-anywhere.herokuapp.com/";
 
 let calUrl = "https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu";
 
+console.log("j'existe ici")
+
 self.addEventListener('install', function(event) {
+    console.log(" on lance installe")
     event.waitUntil(
       caches.open('v1').then(function(cache) {
         return cache.addAll([
@@ -14,12 +17,15 @@ self.addEventListener('install', function(event) {
   });
   
   self.addEventListener('fetch', function(event) {
+      console.log("on lance fetch")
     event.respondWith(caches.match(event.request).then(function(response) {
       // caches.match() always resolves
       // but in case of success response will have value
       if (response !== undefined) {
+        console.log("on passe dans le if")
         return response;
       } else {
+        console.log("on passe dans le else")
         return fetch(event.request).then(function (response) {
           // response may be used only once
           // we need to save clone to put one copy in cache
